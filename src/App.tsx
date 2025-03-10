0e7f91f790f76c5e30e90ef656bd0612c4640dd3
TsioryRasoloarijaona <hei.tsiory@gmail.com>
2025-03-10 11:26:39 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-red-500'>*
+        <p className=' '>hello world</p>
+      </div>
+    </>
+  )
+}
+
+export default App
