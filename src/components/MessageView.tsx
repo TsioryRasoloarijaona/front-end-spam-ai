f13176bfa1f143dcbeba7d87df32e137daa8a18d
TsioryRasoloarijaona <hei.tsiory@gmail.com>
2025-05-16 01:11:34 +0100

    feat:get set messages

--- src/components/MessageView.tsx
+++ src/components/MessageView.tsx
@@ -6,38 +6,100 @@ import Img from "./attachment/Img";
 import Xl from "./attachment/Xl";
 import Translator from "./Translator";
 import { stripHtmlTags } from "@/utils/stringUtils";
+import { SentMessages } from "@/interfaces/SentMessages";
+import { AiOutlineDislike } from "react-icons/ai";
 
-export default function MessageView({ content }: { content: MessageToSend }) {
-  return (
-    <div className=" w-full p-7 space-y-11">
-      <div className="flex justify-between">
-        <div className="flex gap-3 items-center">
-          <div className="bg-[rgb(236,236,240)] text-black font-bold w-[40px] h-[40px] rounded-full flex items-center justify-center uppercase">
-            <p>{`${content.accountDTO?.peopleInfoDTO?.firstName[0]}${content.accountDTO?.peopleInfoDTO?.lastName[0]}`}</p>
+interface MessageViewProps {
+  content?: MessageToSend | null;
+  content1?: SentMessages | null;
+}
+
+export default function MessageView({ content, content1 }: MessageViewProps) {
+  if (content) {
+    return (
+      <div className=" w-full p-7 space-y-5">
+        <div className="flex justify-between items-center">
+          <div className="font-semibold text-sm">
+            object : {content?.object}
           </div>
-          <div className="text-sm">
-            <p>{`${content.accountDTO?.peopleInfoDTO?.firstName} ${content.accountDTO?.peopleInfoDTO?.lastName}`}</p>
-            <p className="font-bold">{content?.accountDTO?.email}</p>
+          <div className="flex items-center gap-4">
+            <button className=" ">
+              <AiOutlineDislike className="text-xl" />
+            </button>
+            <Translator text={stripHtmlTags(content.body)} />
           </div>
         </div>
-        <div>
-          <p className="text-sm">{formatDateTime(content?.sendDateTime)}</p>
+        <div className="flex justify-between">
+          <div className="flex gap-3 items-center">
+            <div className="bg-[rgb(236,236,240)] text-black font-bold w-[40px] h-[40px] rounded-full flex items-center justify-center uppercase">
+              <p>{`${content.accountDTO?.peopleInfoDTO?.firstName[0]}${content.accountDTO?.peopleInfoDTO?.lastName[0]}`}</p>
+            </div>
+            <div className="text-sm">
+              <p>{`${content.accountDTO?.peopleInfoDTO?.firstName} ${content.accountDTO?.peopleInfoDTO?.lastName}`}</p>
+              <p className="font-bold">{content?.accountDTO?.email}</p>
+            </div>
+          </div>
+          <div>
+            <p className="text-sm">{formatDateTime(content?.sendDateTime)}</p>
+          </div>
+        </div>
+        <div
+          className="leading-6 text-sm"
+          dangerouslySetInnerHTML={{ __html: content?.body }}
+        ></div>
+        <div className="flex flex-row gap-3 items-center text-sm">
+          <Pdf name="document.pdf" />
+          <Docs name="document.docx" />
+          <Img name="document.png" />
+          <Xl name="document.xlsx" />
         </div>
       </div>
-      <div className="flex justify-between items-center">
-        <div className="font-semibold text-sm">{content?.object}</div>
-        <Translator text={stripHtmlTags(content.body)}/>
+    );
+  }
+  if (content1) {
+    return (
+      <div className=" w-full p-7 space-y-5">
+        <div className="flex justify-between items-center">
+          <div className="font-semibold text-sm">
+            object : {content1?.object}
+          </div>
+          <Translator text={stripHtmlTags(content1.body)} />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center -space-x-3">
+            {content1.receivers.map((receiver, idx) => (
+              <div
+                key={idx}
+                className="text-white font-bold w-[35px] h-[35px] rounded-full flex items-center justify-center uppercase border-2 border-white text-xs"
+                style={{
+                  backgroundColor: `hsl(${Math.floor(
+                    Math.random() * 360
+                  )}, 50%, 30%)`,
+                  zIndex: content1.receivers.length - idx,
+                }}
+              >
+                <p>{receiver[0]}</p>
+              </div>
+            ))}
+            <span className="ml-4 text-sm font-semibold">
+              {content1.receivers.join(" , ")}
+            </span>
+          </div>
+          <div>
+            <p className="text-sm">{formatDateTime(content1?.sendDateTime)}</p>
+          </div>
+        </div>
+        <div
+          className="leading-6 text-sm"
+          dangerouslySetInnerHTML={{ __html: content1?.body }}
+        ></div>
+        <div className="flex flex-row gap-3 items-center text-sm">
+          <Pdf name="document.pdf" />
+          <Docs name="document.docx" />
+          <Img name="document.png" />
+          <Xl name="document.xlsx" />
+        </div>
       </div>
-      <div
-        className="leading-6 text-sm"
-        dangerouslySetInnerHTML={{ __html: content?.body }}
-      ></div>
-      <div className="flex flex-row gap-3 items-center text-sm">
-        <Pdf name="document.pdf" />
-        <Docs name="document.docx" />
-        <Img name="document.png" />
-        <Xl name="document.xlsx" />
-      </div>
-    </div>
-  );
+    );
+  }
 }
--- src/hooks/webSocketContext.tsx
+++ src/hooks/webSocketContext.tsx
@@ -2,28 +2,36 @@ import React, { createContext, useState, useContext, useRef } from "react";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 import { MessageToSend } from "@/interfaces/dataTypes";
+import { SentMessages } from "@/interfaces/SentMessages";
 
 interface WebSocketContextType {
   messages: MessageToSend[];
+  sentMessages: SentMessages[];
   sendMessage: (message: MessageToSend) => void;
   connectWebSocket: (token: string) => void;
   disconnectWebSocket: () => void;
 }
 const BASE_URL = process.env.SOCKET_URL;
 
-const WebSocketContext = createContext<WebSocketContextType | undefined>(undefined);
+const WebSocketContext = createContext<WebSocketContextType | undefined>(
+  undefined
+);
 
 export const useWebSocket = () => {
-  
   const context = useContext(WebSocketContext);
   if (!context) {
-    throw new Error("useWebSocket doit être utilisé à l'intérieur d'un WebSocketProvider");
+    throw new Error(
+      "useWebSocket doit être utilisé à l'intérieur d'un WebSocketProvider"
+    );
   }
   return context;
 };
 
-export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [messages, setMessages] = useState<MessageToSend[]>([]);
+  const [sentMessages, setSentMessages] = useState<SentMessages[]>([]); // Ajout de l'état pour les messages envoyés
   const stompClientRef = useRef<Stomp.Client | null>(null);
 
   const connectWebSocket = (token: string) => {
@@ -40,11 +48,17 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({ chi
       { Authorization: `Bearer ${token}` },
       () => {
         console.log("✅ WebSocket connecté !");
-        
+
         stompClient.subscribe("/user/queue/messages", (message) => {
           const newMessage = JSON.parse(message.body);
           setMessages((prevMessages) => [newMessage, ...prevMessages]);
           console.log("📩 Message reçu :", newMessage);
+        });
+
+        stompClient.subscribe("/user/queue/sent", (message) => {
+          const sentMessage: SentMessages = JSON.parse(message.body);
+          setSentMessages((prev) => [sentMessage, ...prev]); 
+          console.log("✉️ Sent message reçu :", sentMessage);
         });
       },
       (error) => {
@@ -55,7 +69,11 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({ chi
 
   const sendMessage = (message: MessageToSend) => {
     if (stompClientRef.current && stompClientRef.current.connected) {
-      stompClientRef.current.send("/app/chat/private", {}, JSON.stringify(message));
+      stompClientRef.current.send(
+        "/app/chat/private",
+        {},
+        JSON.stringify(message)
+      );
       console.log("📤 Message envoyé :", message);
     } else {
       console.error("🚫 Impossible d'envoyer, WebSocket non connecté.");
@@ -72,7 +90,15 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({ chi
   };
 
   return (
-    <WebSocketContext.Provider value={{ messages, sendMessage, connectWebSocket, disconnectWebSocket }}>
+    <WebSocketContext.Provider
+      value={{
+        messages,
+        sentMessages,
+        sendMessage,
+        connectWebSocket,
+        disconnectWebSocket,
+      }}
+    >
       {children}
     </WebSocketContext.Provider>
   );
